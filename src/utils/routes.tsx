fed0d2c55667d73e4816081965b0e505da8a1d9b
Pranciškus Ambrazas <p.ambrazas@gmail.com>
2023-12-28 15:19:40 +0200

    Revert "Prisijungimo langas"

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -1,17 +1,11 @@
 export const slugs = {
-  login: '/login',
-  forgotPassword: '/pamirsau',
-  resetPassword: '/atstatyti',
-  invite: '/pakvietimas',
-  users: '/naudotojai',
-  profile: '/profilis',
+  page: '/page',
 };
 
 export const routes = [
   {
-    name: 'Naudotojai',
-    slug: slugs.users,
-    sidebar: true,
-    component: <div>Naudotojai</div>,
+    label: 'page',
+    slug: slugs.page,
+    component: <div>page</div>,
   },
 ];
