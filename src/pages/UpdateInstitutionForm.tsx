aac5da5f53709bf12bf9a9c9f044fb6732493d08
LWangllix <vanglikas.lukas@gmail.com>
2023-12-29 18:59:14 +0200

    permission fixes

--- src/pages/UpdateInstitutionForm.tsx
+++ src/pages/UpdateInstitutionForm.tsx
@@ -2,39 +2,26 @@ import { useMutation, useQuery } from 'react-query';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import * as Yup from 'yup';
 import OrganizationForm from '../components/forms/OrganizationForm';
-import UserWithEmailForm from '../components/forms/UserWithEmailForm';
 import FormPageWrapper from '../components/layouts/FormLayout';
+import FullscreenLoader from '../components/other/FullscreenLoader';
 import { Column } from '../styles/CommonStyles';
-import { ReactQueryError } from '../types';
+import { DeleteInfoProps, ReactQueryError } from '../types';
 import Api from '../utils/api';
-import { TenantTypes } from '../utils/constants';
 import { getReactQueryErrorMessage, handleErrorToastFromServer, isNew } from '../utils/functions';
 import { slugs } from '../utils/routes';
-import { pageTitles, validationTexts } from '../utils/texts';
+import {
+  buttonsTitles,
+  deleteDescriptionFirstPart,
+  deleteDescriptionSecondPart,
+  deleteTitles,
+  pageTitles,
+  validationTexts,
+} from '../utils/texts';
 
 import { companyCode } from 'lt-codes';
+import { TenantTypes } from '../utils/constants';
 
 export const validateCreateUserForm = Yup.object().shape({
-  firstName: Yup.string()
-    .required(validationTexts.requireText)
-    .test('validFirstName', validationTexts.validFirstName, (values) => {
-      if (/\d/.test(values || '')) return false;
-
-      return true;
-    }),
-  lastName: Yup.string()
-    .required(validationTexts.requireText)
-    .test('validLastName', validationTexts.validLastName, (values) => {
-      if (/\d/.test(values || '')) return false;
-
-      return true;
-    }),
-  phone: Yup.string()
-    .required(validationTexts.requireText)
-    .trim()
-    .matches(/^(86|\+3706)\d{7}$/, validationTexts.badPhoneFormat),
-  email: Yup.string().email(validationTexts.badEmailFormat).required(validationTexts.requireText),
-
   companyName: Yup.string().required(validationTexts.requireText).trim(),
   companyCode: Yup.string()
     .required(validationTexts.requireText)
@@ -56,13 +43,8 @@ export interface InstitutionProps {
   companyCode?: string;
   companyPhone?: string;
   address?: string;
-  firstName?: string;
+  canHaveChildren: boolean;
   companyEmail: string;
-  lastName?: string;
-  email?: string;
-  personalCode?: string;
-  phone: string;
-  canHaveChildren?: boolean;
   parent?: any;
   data?: {
     url: string;
@@ -75,72 +57,50 @@ export interface InstitutionProps {
   };
 }
 
-const UserForm = () => {
+const UpdateOrganizationForm = () => {
   const navigate = useNavigate();
   const { id = '' } = useParams();
   const [searchParams] = useSearchParams();
   const { parent } = Object.fromEntries([...Array.from(searchParams)]);
 
-  const title = pageTitles.newInstitution;
+  const title = pageTitles.updateInstitution;
+
+  const { isFetching, data: institution } = useQuery(
+    ['institution', id],
+    () => Api.getTenant({ id }),
+    {
+      onError: () => {
+        navigate(slugs.institutions);
+      },
+    },
+  );
 
   const handleSubmit = async (values: InstitutionProps, { setErrors }) => {
-    const {
-      firstName,
-      lastName,
-      email,
-      phone,
-      companyEmail,
-      companyCode,
-      personalCode,
-      companyName,
-      companyPhone,
-      parent,
-      canHaveChildren,
-      data,
-    } = values;
+    const { companyEmail, companyCode, companyName, companyPhone, parent, data } = values;
 
     const params = {
-      user: {
-        firstName,
-        lastName,
-        email: email?.toLowerCase(),
-        phone,
-        personalCode,
-      },
       name: companyName,
       code: companyCode,
       phone: companyPhone,
       email: companyEmail?.toLowerCase(),
       ...(!!parent && { parent: parseInt(parent) }),
-      tenantType: canHaveChildren ? TenantTypes.ORGANIZATION : TenantTypes.MUNICIPALITY,
       data,
     };
 
-    if (isNew(id)) {
-      try {
-        await createTenant.mutateAsync(params);
-      } catch (e: any) {
-        const error = e as ReactQueryError;
-        const errorMessage = getReactQueryErrorMessage(error.response.data.message);
-        setErrors({ email: errorMessage });
-      }
-      return;
+    try {
+      return await updateTenant.mutateAsync(params);
+    } catch (e: any) {
+      const error = e as ReactQueryError;
+      const errorMessage = getReactQueryErrorMessage(error.response.data.message);
+      setErrors({ email: errorMessage });
     }
   };
 
-  const createTenant = useMutation((params: any) => Api.createTenant({ params }), {
+  const updateTenant = useMutation((params: any) => Api.updateTenant({ params, id }), {
     onError: ({ response }) => {
       handleErrorToastFromServer(response);
     },
-    onSuccess: (response) => {
-      if (response?.url) {
-        const url = new URL(response?.url);
-        url.hostname = window.location.hostname;
-        url.port = window.location.port;
-        url.protocol = window.location.protocol;
-        console.log(url.href);
-        alert(url.href);
-      }
+    onSuccess: () => {
       navigate(slugs.institutions);
     },
     retry: false,
@@ -156,35 +116,57 @@ const UserForm = () => {
     },
   );
 
+  const handleDelete = useMutation(
+    () =>
+      Api.deleteTenant({
+        id,
+      }),
+    {
+      onError: () => {
+        handleErrorToastFromServer();
+      },
+      onSuccess: () => {
+        navigate(slugs.institutions);
+      },
+    },
+  );
+
+  const deleteInfo: DeleteInfoProps = {
+    deleteButtonText: buttonsTitles.group,
+    deleteDescriptionFirstPart: deleteDescriptionFirstPart.group,
+    deleteDescriptionSecondPart: deleteDescriptionSecondPart.group,
+    deleteTitle: deleteTitles.group,
+    deleteName: institution?.name,
+    handleDelete: !isNew(id) ? handleDelete.mutateAsync : undefined,
+  };
+
   const initialValues: InstitutionProps = {
-    companyName: '',
-    companyCode: '',
-    companyPhone: '',
-    companyEmail: '',
-    firstName: '',
-    lastName: '',
-    address: '',
-    email: '',
-    canHaveChildren: false,
-    phone: '',
-    personalCode: '',
-    parent: parent || '',
+    companyName: institution?.name || '',
+    companyCode: institution?.code || '',
+    companyPhone: institution?.phone || '',
+    companyEmail: institution?.email || '',
+    address: institution?.address || '',
+    parent: institution?.parent || parent || '',
+    canHaveChildren: institution?.tenantType === TenantTypes.ORGANIZATION,
+    data: { ...institution?.data },
   };
 
   const renderForm = (values: InstitutionProps, errors: any, handleChange) => {
     return (
       <Column>
         <OrganizationForm
-          toggleCanHaveChildren={true}
           values={values}
           errors={errors}
           handleChange={handleChange}
           groupOptions={groupOptions}
         />
-        <UserWithEmailForm values={values} errors={errors} handleChange={handleChange} />
       </Column>
     );
   };
+
+  if (isFetching) {
+    return <FullscreenLoader />;
+  }
 
   return (
     <FormPageWrapper
@@ -193,8 +175,9 @@ const UserForm = () => {
       onSubmit={handleSubmit}
       renderForm={renderForm}
       validationSchema={validateCreateUserForm}
+      deleteInfo={deleteInfo}
     />
   );
 };
 
-export default UserForm;
+export default UpdateOrganizationForm;
