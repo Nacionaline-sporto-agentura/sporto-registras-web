407d9c1af3ae3781cb77c88395ea77d1d1ae9bde
LWangllix <vanglikas.lukas@gmail.com>
2024-06-06 13:56:04 +0300

    fix

--- manifest.js
+++ manifest.js
@@ -1,3 +1,5 @@
+const VITE_BASE_URL = import.meta.env?.VITE_BASE_URL || '/';
+
 export const manifestForPlugIn = {
   registerType: 'autoUpdate',
   includeAssests: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
@@ -10,25 +12,25 @@ export const manifestForPlugIn = {
     description: 'Sporto Registras',
     icons: [
       {
-        src: '/android-chrome-192x192.png',
+        src: `${VITE_BASE_URL}/android-chrome-192x192.png`,
         sizes: '192x192',
         type: 'image/png',
         purpose: 'favicon',
       },
       {
-        src: '/android-chrome-512x512.png',
+        src: `${VITE_BASE_URL}/android-chrome-512x512.png`,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'favicon',
       },
       {
-        src: '/apple-touch-icon.png',
+        src: `${VITE_BASE_URL}/apple-touch-icon.png`,
         sizes: '180x180',
         type: 'image/png',
         purpose: 'apple touch icon',
       },
       {
-        src: '/maskable_icon.png',
+        src: `${VITE_BASE_URL}/maskable_icon.png`,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any maskable',
